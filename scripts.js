ab3c623dd4085311692be2673a5626a6ec290015
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-01-29 17:03:48 -0600

    stored name values

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-console.log('js in linked');
+$(document).ready(andGo);
+
+let firstName;
+let lastName;
+let employee = {};
+
+function andGo() {
+  // get info from form
+  firstName = $('#First-Name-Input');
+  lastName = $('#Last-Name-Input');
+  // const employeeID = $('')
+  $('#submit-button-input').on('click', submitForm);
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  const firstNameVal = firstName.val();
+  const lastNameVal = lastName.val();
+  console.log(firstNameVal, lastNameVal);
+}
