1177de3aa961b8f13960b0b6b1f77f657e79f5cc
Josh Gulledge <joshcalebgulledge@gmail.com>
2021-01-30 13:29:15 -0600

    delete function works

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 ## Description
 
-Your project description goes here. What problem did you solve? How did you solve it?
+This project was to take in an organizations employee list, and to with that track the monthly spending on employee salary with that information.
+
+- Add your employee and salary information
+- Have employees listed
+- Delete employee information
+- Get a monthly cost of all employees
 
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
--- scripts.js
+++ scripts.js
@@ -5,7 +5,42 @@ let monthlyCost = 0;
 
 function andGo() {
   $('#submit-button-input').on('click', submitForm);
+  $('#delete-btn').on('click', deleteEmployee);
 }
+
+function deleteEmployee() {
+  // get the inputs
+  const empDltInput = $('#employee-delete-input');
+  const empIDDltInput = $('#employeeID-delete-input');
+
+  // find the right object with given inputs
+  const { firstName, employeeID, ...rest } = searchEmployeeList(
+    empDltInput.val()
+  );
+
+  // find the right object in the array
+  const indexWeWant = employeeList.findIndex(
+    (obj) => obj.firstName === firstName
+  );
+
+  // remove that obj from the array
+  employeeList.splice(indexWeWant, 1);
+
+  // empty existing employee table
+  $('.employee-info').empty();
+  // render table with new list
+  employeeList.forEach((emplObj) => {
+    renderData(emplObj);
+  });
+} // end deleteEmployee
+
+function searchEmployeeList(emply) {
+  // console.log(emply, empID);
+  const [iFoundYou] = employeeList.filter((emp) => {
+    return emp.firstName === emply;
+  });
+  return iFoundYou;
+} // end searchEmployeeList
 
 function submitForm(e) {
   e.preventDefault();
@@ -20,7 +55,6 @@ function submitForm(e) {
 
   // push obj to array
   employeeList.push(employee);
-  console.log(employeeList);
 
   // ----clear the input fields
   $('#First-Name-Input').val('');
@@ -34,17 +68,17 @@ function submitForm(e) {
 
   // put data on table
   renderData(employee);
-}
+} // end submitForm
 
 function calMonthCost(annualSalary) {
   // get monthly cost hence 12
-  monthlyCost = annualSalary / 12;
+  monthlyCost += annualSalary / 12;
 
   // check if over 20000 a month
-  if (monthlyCost > 20) {
-    $('#monthly-cost-output').css('backgound-color: red');
+  if (monthlyCost > 20000) {
+    $('#monthly-cost-output').css('background-color', 'red');
   }
-}
+} // end calMonthCost
 
 function renderData(employeeObj) {
   $('.employee-info').append(`
@@ -58,4 +92,4 @@ function renderData(employeeObj) {
   `);
 
   $('#monthly-cost-output').empty().append(`$${monthlyCost}`);
-}
+} // end renderData
